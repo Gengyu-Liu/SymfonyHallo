6ecbb222a99d02e12b16d211f3160245785276ff
gyliu <liugengyu@hotmail.com>
2021-07-29 21:19:52 +0200

    Route parameters and their validation, request Object, redirect configuration without a custom controller, rendering a template directly from a route

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,24 +1,37 @@
 <?php
-
+/**
+ * Route Parameters and their validation
+ * Getting the Route Name and Parameters from Request Object
+ */
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
+#[Route('/blog', name: 'blog_')]
 class BlogController extends AbstractController{
-    #[Route('/blog/{page<\d+>?1}', name: 'blog_list')]
-    public function list(int $page = 1): Response
+    #[Route('/{page<\d+>?1}', name: 'list')]
+    public function list(int $page, Request $request): Response
     {
-       return new Response(
-               '<html><body>List methode mit '.$page.'</body></html>'
-       );
+//        $routeName = $request->attributes->get('_route');
+//        $routeParameters = $request->attributes->get('_route_params');
+//        // use this to get all the available attributes (not only routing ones):
+//        $allAttributes = $request->attributes->all();
+
+        
+        return $this->render('blog/list.html.twig', [
+            'page' => $page,
+        ]);
+//        return new Response(
+//               '<html><body>List methode mit '.$page.'</body></html>'
+//       );
     }
     
-    #[Route('/blog/{start}/{end}', name: 'blog_diff')]
+    #[Route('/{start}/{end}', name: 'diff')]
     #[ParamConverter('start', options: ['format' => '!Y-m-d'])]
     #[ParamConverter('end', options: ['format' => '!Y-m-d'])]
     public function diff(\DateTime $start, \DateTime $end): Response
